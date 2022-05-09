f66e2a61d6db7f4edfc992e16e03ec952ce45686
MICHEAL EMMANUEL <meeckeytek@gmail.com>
2022-05-10 00:25:50 +0100

    subscribe added

--- server/app.js
+++ server/app.js
@@ -1,31 +1,71 @@
-const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
-const dotenv = require("dotenv");
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const { execute, subscribe } = require('graphql');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const gql = require('graphql-tag');
+const express = require('express');
+const http = require('http');
+const { PubSub } = require('graphql-subscriptions');
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+const connectDatabase = require("./middleware/db")
 dotenv.config();
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-// const pubsup = new PubSub();
+(async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const pubsub = new PubSub();
+  
+  
+  const server = new ApolloServer({
+      schema,
+      context: ({ req, res }) => ({ req, res, pubsub }),
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), {
+          async serverWillStart() {
+              return {
+                  async drainServer() {
+                      subscriptionServer.close();
+                  }
+              };
+          }
+      }],
+  });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  // context: ({req}) => ({req, pubsup})
-  context: ({req}) => ({req})
+
+const subscriptionServer = SubscriptionServer.create({
+  schema,
+  execute,
+  subscribe,
+  async onConnect(
+      connectionParams,
+      webSocket,
+      context
+  ) {
+      console.log('Connected!');
+      return {
+          pubsub
+      }
+  },
+  onDisconnect(webSocket, context) {
+      console.log('Disconnected!')
+  },
+}, {
+  server: httpServer,
+  path: server.graphqlPath,
 });
 
-mongoose
-  .connect(process.env.CONNECTIONURI, { useNewUrlParser: true })
-  .then(() => {
-    return server
-      .listen({ port: 3001 })
-      .then((res) => {
-        console.log(`Server running at ${res.url}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+// More required logic for integrating with Express
+await server.start();
+server.applyMiddleware({app});
+
+// Modified server startup
+connectDatabase();
+await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+})(typeDefs, resolvers);
